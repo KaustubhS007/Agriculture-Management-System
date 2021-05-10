de43847fcebba570dd226ed96210752c6851697e
KaustubhS007 <70136327+KaustubhS007@users.noreply.github.com>
2021-05-10 12:04:25 +0530

    Update app.py

--- DBMS MP/app.py
+++ DBMS MP/app.py
@@ -14,7 +14,7 @@ app.secret_key = os.urandom(24)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'cricket@123'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'eagr'
 #app.config['MYSQL_PORT']='3307'
 
